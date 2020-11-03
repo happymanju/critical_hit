8280150c3bed1b543198b37d966577b36e6b5f32
Michael Piastucki <44167171+happymanju@users.noreply.github.com>
2020-11-03 09:59:25 +0900

    added first migration and knexfile

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      filename: './dev.pg'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ migrations/20201103095239_initial-migration.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("entities", function(table) {
+        table.increments(id);
+        table.string('name', 255).notNullable();
+        table.integer("initiative", 255);
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTable("entities");
+  
+};
