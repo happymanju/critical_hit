0434df8fb1124dbd7fdae712a3af87c537a176d4
Michael Piastucki <44167171+happymanju@users.noreply.github.com>
2020-11-03 11:47:34 +0900

    added Entity component

---
+++ src/components/Entity.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="entity">
+        <p>Name:</p>
+        <p>test name</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Entity",
+
+}
+</script>
+
+<style>
+.entity {
+    border: 1px solid #4444;
+}
+</style>
